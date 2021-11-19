25857532b4f200bc952104f1f6f56914cb45ed34
capntrips <31455058+capntrips@users.noreply.github.com>
2021-11-19 10:28:52 -0600

    fix early exception handling

--- app/src/main/java/com/github/capntrips/devinfopatcher/MainViewModel.kt
+++ app/src/main/java/com/github/capntrips/devinfopatcher/MainViewModel.kt
@@ -11,7 +11,7 @@ import kotlinx.coroutines.launch
 
 class MainViewModel constructor(context: Context) : ViewModel(), MainViewModelInterface {
     private val _isRefreshing: MutableStateFlow<Boolean> = MutableStateFlow(false)
-    private var _uiState: MutableStateFlow<DeviceStateInterface> = MutableStateFlow(DeviceState(context, _isRefreshing))
+    private lateinit var _uiState: MutableStateFlow<DeviceStateInterface>
     private var _error: String? = null
 
     override val isRefreshing: StateFlow<Boolean>
@@ -22,6 +22,14 @@ class MainViewModel constructor(context: Context) : ViewModel(), MainViewModelIn
         get() = _error != null
     val error: String
         get() = _error ?: "Unknown Error"
+
+    init {
+        try {
+            _uiState = MutableStateFlow(DeviceState(context, _isRefreshing))
+        } catch (e: Exception) {
+            _error = e.message
+        }
+    }
 
     private fun launch(block: suspend () -> Unit) {
         viewModelScope.launch(Dispatchers.IO) {
